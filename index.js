b137252e44a3e000dbe0ddc496362c76c2b030da
Haidong Wang <beavermelon@gmail.com>
2017-09-15 00:57:29 +0900

    add sample koa code

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 'use strict';
 
+const Koa = require('koa');
+const app = new Koa();
 
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
+app.use(ctx => {
+    ctx.body = 'hello koa2';
+});
+
+app.listen(8000);
